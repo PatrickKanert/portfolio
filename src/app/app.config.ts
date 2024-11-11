e6a3e262a1f299e170547942c30331cf4b88edf0
PatrickKanert <patrickkanert@gmail.com>
2024-11-11 20:43:00 +0100

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"portfolio-52375","appId":"1:163058226139:web:94dc8f680da98c6b0a4dfe","storageBucket":"portfolio-52375.firebasestorage.app","apiKey":"AIzaSyDZlsrkMMHv__LelsK41_QI9RXHe05kZH0","authDomain":"portfolio-52375.firebaseapp.com","messagingSenderId":"163058226139"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
