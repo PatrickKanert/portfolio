cca9e0dd168b6b678961dd0cb088a6d8c314e4f7
PatrickKanert <patrickkanert@gmail.com>
2024-12-28 13:28:20 +0100

    added skills section

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss',
 })
-export class AboutMeComponent {}
+export class AboutMeComponent {
+  isHovered = false;
+
+  // Methode, die aufgerufen wird, wenn die Maus über das Bild fährt
+  onMouseEnter() {
+    this.isHovered = true;
+  }
+
+  onMouseLeave() {
+    // Du kannst diese Methode leer lassen oder entfernen,
+    // wenn du willst, dass der Hintergrund auch nach dem Verlassen der Maus bleibt
+  }
+}
