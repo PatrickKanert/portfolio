eeab2c29f5a37e264eb6ce00c403bf4eb59d205c
PatrickKanert <patrickkanert@gmail.com>
2025-01-15 14:10:00 +0100

    header made responsive

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { RouterModule } from "@angular/router";
 import { TranslatePipe, TranslateService } from "@ngx-translate/core";
@@ -7,11 +8,20 @@ import { TranslatePipe, TranslateService } from "@ngx-translate/core";
 @Component({
 	selector: "app-header",
 	standalone: true,
-	imports: [CommonModule, MatSlideToggleModule, TranslatePipe, RouterModule],
+	imports: [
+		CommonModule,
+		MatSlideToggleModule,
+		TranslatePipe,
+		RouterModule,
+		MatIconModule,
+	],
 	templateUrl: "./header.component.html",
 	styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
+	isSmallScreen: boolean = window.innerWidth <= 799;
+	isMenuOpen = false;
+
 	constructor(private translate: TranslateService) {
 		this.translate.addLangs(["de", "en"]);
 		this.translate.setDefaultLang("en");
@@ -20,5 +30,14 @@ export class HeaderComponent {
 	toggleLanguage(event: Event) {
 		const checked = (event.target as HTMLInputElement).checked;
 		this.translate.use(checked ? "de" : "en"); // Sprache umschalten
+	}
+
+	@HostListener("window:resize", ["$event"])
+	onResize(event: Event): void {
+		this.isSmallScreen = window.innerWidth <= 799;
+	}
+
+	toggleMenu(): void {
+		this.isMenuOpen = !this.isMenuOpen; // Zustand umschalten
 	}
 }
