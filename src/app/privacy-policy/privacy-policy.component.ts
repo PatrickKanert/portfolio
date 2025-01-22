ecafd3c1e0502cc1be22798de17da854caaa7195
PatrickKanert <patrickkanert@gmail.com>
2025-01-22 11:52:54 +0100

    made privacy-policy responsive

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,16 +1,21 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
 import { TranslateModule, TranslatePipe } from "@ngx-translate/core";
 
 @Component({
 	selector: "app-privacy-policy",
 	standalone: true,
-	imports: [CommonModule, TranslatePipe, TranslateModule],
+	imports: [CommonModule, TranslatePipe, TranslateModule, MatIconModule],
 	templateUrl: "./privacy-policy.component.html",
 	styleUrl: "./privacy-policy.component.scss",
 })
 export class PrivacyPolicyComponent implements OnInit {
 	ngOnInit(): void {
 		window.scrollTo({ top: 0, behavior: "smooth" }); // Scrollt zur Oberseite
+	}
+
+	goBack(): void {
+		window.history.back();
 	}
 }
